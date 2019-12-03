86133a3f1eb892c5a02d563399cadd1f82909a50
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-12-03 12:40:14 +0900

    Fix notificatrion

--- server/middlewares/notifications.js
+++ server/middlewares/notifications.js
@@ -1,23 +1,29 @@
 const axios = require('axios');
 
-const sendPushNotificationByAxios = async (pushToken, title, message) => {
-  return await axios.post(
-    'https://exp.host/--/api/v2/push/send',
-    {
-      to: pushToken,
-      sound: 'default',
-      title: title,
-      body: message
-    },
-    {
-      headers: {
-        accept: 'application/json',
-        'accept-encoding': 'gzip, deflate',
-        'content-type': 'application/json'
+const sendPushNotificationByAxios = (pushToken, title, message) => {
+  return axios
+    .post(
+      'https://exp.host/--/api/v2/push/send',
+      {
+        to: pushToken,
+        sound: 'default',
+        title: title,
+        body: message
+      },
+      {
+        headers: {
+          accept: 'application/json',
+          'accept-encoding': 'gzip, deflate',
+          'content-type': 'application/json'
+        }
       }
-    }
-  );
+    )
+    .catch(err => {
+      console.error(err);
+      throw err;
+    });
 };
+
 module.exports = {
   sendPushNotificationByAxios
 };
