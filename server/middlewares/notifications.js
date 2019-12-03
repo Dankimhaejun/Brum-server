da35b4bc1f2cae1f74b13562fdff38f1cd9332f7
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-12-03 12:39:48 +0900

    Delete catch.error

--- server/middlewares/notifications.js
+++ server/middlewares/notifications.js
@@ -1,26 +1,22 @@
 const axios = require('axios');
 
 const sendPushNotificationByAxios = async (pushToken, title, message) => {
-  return await axios
-    .post(
-      'https://exp.host/--/api/v2/push/send',
-      {
-        to: pushToken,
-        sound: 'default',
-        title: title,
-        body: message
-      },
-      {
-        headers: {
-          accept: 'application/json',
-          'accept-encoding': 'gzip, deflate',
-          'content-type': 'application/json'
-        }
+  return await axios.post(
+    'https://exp.host/--/api/v2/push/send',
+    {
+      to: pushToken,
+      sound: 'default',
+      title: title,
+      body: message
+    },
+    {
+      headers: {
+        accept: 'application/json',
+        'accept-encoding': 'gzip, deflate',
+        'content-type': 'application/json'
       }
-    )
-    .catch(err => {
-      throw err;
-    });
+    }
+  );
 };
 module.exports = {
   sendPushNotificationByAxios
